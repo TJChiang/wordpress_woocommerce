a1ac037ca170027648f15c6a60ca63b09846cc96
TJChiang <tjchiang8419@gmail.com>
2025-01-16 10:56:31 +0800

    setup k6 + grafana + prometheus

--- k6/stress_test.js
+++ k6/stress_test.js
@@ -9,8 +9,10 @@ export const options = {
     ],
 };
 
+const BASE_URL = __ENV.BASE_URL !== undefined ? __ENV.BASE_URL : 'http://localhost/';
+
 export default function () {
-    const res = http.get('http://localhost/');
+    const res = http.get(`${BASE_URL}`);
     check(res, {
         'status is 200': (r) => r.status === 200,
         'response time < 500ms': (r) => r.timings.duration < 500,
